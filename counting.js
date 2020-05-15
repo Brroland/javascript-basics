2e66de6dd01b94f3fe6f85aa038337b64d07c0ad
Braj Roland <brroland2@gmail.com>
2020-05-15 23:32:48 +0200

    development push with comment

--- counting.js
+++ counting.js
@@ -1,4 +1,4 @@
-const countElement = (arr, element) => {
+const countElement = (arr, element) => {	// development comment for commit
     let count =0;
     for (let i=0; i < arr.length; i++){
         if (arr[i] === element) {
