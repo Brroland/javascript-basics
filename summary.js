32cf8b9e6f5f8bc57746bc76fcf24fe56f49ad3a
Braj Roland <brroland2@gmail.com>
2020-05-15 23:24:32 +0200

    adding summary.js to development with comment

--- summary.js
+++ summary.js
@@ -1,4 +1,4 @@
-const sumArray = (arr) => {
+const sumArray = (arr) => {	// first comment and must have comment on development
     let sum = 0;
     for(const element of arr) {
         sum+= element;
