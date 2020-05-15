f63922df8813b562e179d1d7aa279d2c8eed932f
Braj Roland <brroland2@gmail.com>
2020-05-16 00:11:48 +0200

    making union from 2 arrays

---
+++ unio.js
@@ -0,0 +1,15 @@
+const union = (src1, src2) => {
+  const out = [];
+  for (const element of src1) { // Másolás tétel feltétel nélkül
+    out.push(element);
+  }
+  for (let i = 0; i < src2.length; i++) {
+    if (!out.includes(src2[i])) {
+      out.push(src2[i]);
+    }
+  }
+  return out;
+};
+module.exports {
+	union
+}
