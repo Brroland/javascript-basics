01e2cabb70feb1a6419979d36a81769e10058fb1
Braj Roland <brroland2@gmail.com>
2020-05-15 23:49:46 +0200

    decide algorythm

---
+++ decide.js
@@ -0,0 +1,8 @@
+const containsElement = (arr, element) => {	// decide if the 'element' is exist in the array
+  for (const actual of arr) {
+    if (actual === element) {
+      return true;
+    }
+  }
+  return false;
+};
