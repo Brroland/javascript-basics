841484c1f49c58b84a63216294ea302573dfb583
Braj Roland <brroland2@gmail.com>
2020-05-16 00:03:35 +0200

    selecting even numbers with algor.

---
+++ select.js
@@ -0,0 +1,13 @@
+const selectEvens = (src) => {
+  const out = [];
+  let j = 0;
+  for (let i = 0; i < src.length; i++) {
+    if (src[i] % 2 === 0) {
+      out[j++] = src[i]; // out.push(src[i]);
+    }
+  }
+  return out;
+};
+module.exports {
+	selectEvens
+}
