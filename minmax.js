d322e4c85d0929e32d8e9924dbf82c08d9872465
Braj Roland <brroland2@gmail.com>
2020-05-15 23:46:16 +0200

    min and max algorythms

---
+++ minmax.js
@@ -0,0 +1,25 @@
+const maxArr = (arr) => {	// maximum
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+};
+
+const minArr = (arr) => {       // minimum
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
+};
+
+module.exports {
+	max : maxArr,
+	min : minArr
+}
+
