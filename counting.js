0285fefd7177ef4be416a55e0915fb29bd373f29
Braj Roland <brroland2@gmail.com>
2020-05-15 22:31:42 +0200

    created the file and filled with the code

---
+++ counting.js
@@ -0,0 +1,10 @@
+const countElement = (arr, element) => {
+    let count =0;
+    for (let i=0; i < arr.length; i++){
+        if (arr[i] === element) {
+            count++;
+        }
+
+    }
+    return count;
+};
